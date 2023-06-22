70c487f7d03cbb1aa261792ea8aa39061fa92045
Pierce Putz <git@putz.space>
2023-06-22 17:11:44 -0500

    Finish chapter 12

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
 // index.js
 // This is the main entry point of our application
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from '/pages';         //import routes
+
+const App = () => {
+    return(
+        <div>
+            <Pages />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">
+                        <span aria-hidden="true" role="img">
+                            🏠️
+                        </span> Home</Link>
+                </li>
+                <li>
+                    <Link to="/mynotes">
+                        <span aria-hidden="true" role="img">
+                            📒️
+                        </span> My Notes</Link>
+                </li>
+                <li>
+                    <Link to="/favorites">
+                        <span aria-hidden="true" role="img">
+                            ⭐️
+                        </span> Favorites</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
